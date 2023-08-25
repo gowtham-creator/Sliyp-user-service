2f36053b0ff95adacf6623b857a7f2c7a5adf14b
shashi <mallepally.varma@jsw.in>
2023-08-25 11:43:04 +0530

    cors fixed

--- src/main/java/com/slip/user/config/WebSocketConfig.java
+++ src/main/java/com/slip/user/config/WebSocketConfig.java
@@ -2,7 +2,6 @@ package com.slip.user.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
@@ -12,6 +11,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new WebSocketHandler(), "/ws/chat").setAllowedOrigins("*");
+        registry.addHandler(new WebSocketHandler(), "/ws/chat")
+                .setAllowedOrigins("*");
     }
 }
