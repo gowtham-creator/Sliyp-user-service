a113d86942c6d70eb52226b9898aed2a7765b234
shashi <mallepally.varma@jsw.in>
2023-09-11 19:00:51 +0530

    user created mail

--- src/main/java/com/slip/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/slip/user/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.slip.user.service.impl;
 
 import com.slip.user.Models.User;
 import com.slip.user.repositories.UserRepository;
+import com.slip.user.service.EmailService;
 import com.slip.user.service.UserService;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +13,10 @@ import java.util.Objects;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-    public UserServiceImpl(UserRepository userRepository) {
+    private final EmailService emailService;
+    public UserServiceImpl(UserRepository userRepository, EmailService emailService) {
         this.userRepository = userRepository;
+        this.emailService = emailService;
     }
 
    @Override
@@ -25,7 +28,9 @@ public class UserServiceImpl implements UserService {
         if(!Objects.isNull(userRepository.getUserByEmail(user.getEmail()))){
         throw new RuntimeException("User already exists");
         }
-        return userRepository.save(user);
+        User userCreated= userRepository.save(user);
+        emailService.sendEmail(userCreated.getEmail(),"SLiYp User Created","Hii "+user.getName()+",\n Your  SLiYp  account has created successfully");
+        return userCreated;
     }
     @Override
     public User getUserByHandle(String handle){
