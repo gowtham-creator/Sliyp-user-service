8e92590045c0fcdf37788ea9bb68ceba58cef3b8
shashi <mallepally.varma@jsw.in>
2023-09-15 23:33:52 +0530

    fix cors error

--- src/main/java/com/slip/user/repositories/PostRepository.java
+++ src/main/java/com/slip/user/repositories/PostRepository.java
@@ -5,14 +5,18 @@ import com.slip.user.Models.User;
 
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends Neo4jRepository<Post,Long> {
-//    @Query("")
-//    void createHasPostedRelation(String userId , Long postId);
+
+//    public final String CREATE_USER_POST_RELATIONSHIP = "CREATE (user:User {email: ~$email})-[hasPosted:HAS_POSTED]->(post:Post {id: ~$postId}) RETURN type(hasPosted)";
+//    public final  String CHECK_USER_POST_RELATIONSHIP = "MATCH (user:User {email: ~$email})-[hasPosted:HAS_POSTED]->(post:Post {id: ~$postId}) return hasPosted is not null";
 //
-//    @Query("")
-//    Boolean hasPostedRelationExist(String userId , Long postId);
+//    @Query(CREATE_USER_POST_RELATIONSHIP)
+//    String createHasPostedRelation(@Param("email") String email ,@Param("postID") Long  postId);
+//    @Query(CHECK_USER_POST_RELATIONSHIP)
+//    Boolean hasPostedRelationExist(@Param("email") String email  ,@Param("postID") Long  postId);
 
 }
